fd4203be37b02bc8141b6221423ad97430c85f39
hao.xie <hao.xie@hzztai.com>
2018-09-28 19:04:38 +0800

    add eslint

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -18,7 +18,7 @@ module.exports = function () {
             app: ['./src/main.js'],
         },
         resolve: {
-            extensions: ['.js','.vue','.json'],
+            extensions: ['.js', '.vue', '.json'],
             alias: {
                 'vue$': 'vue/dist/vue.esm.js',
                 '@': root('src'),
